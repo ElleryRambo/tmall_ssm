f4b5c80a8f94d2b7aa34fd6ad37fc7b52e672383
Ellery <554156000@qq.com>
2019-12-25 12:45:40 +0800

    实现后台类别下的属性管理，包括增删改查

---
+++ src/main/java/com/wgh/tmall/service/PropertyService.java
@@ -0,0 +1,18 @@
+package com.wgh.tmall.service;
+
+import com.wgh.tmall.pojo.Property;
+
+import java.util.List;
+
+public interface PropertyService {
+
+    void add(Property p);
+
+    void delete(int id);
+
+    void update(Property p);
+
+    Property get(int id);
+
+    List list(int cid);
+}
